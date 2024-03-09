fafe0c3090ad6af8f029981fbbd1f16554aad150
Dianne Corpuz <59422083+corpzbrideee02@users.noreply.github.com>
2024-03-08 22:50:53 -0500

    added more photography & nature pics

--- src/json/natureList.js
+++ src/json/natureList.js
@@ -7,11 +7,23 @@ export const nature_list = [
     { alt: "Snail", src: 6, id: 6 },
     { alt: "Boats in Deep River, Ontario", src: 7, id: 7 },
     { alt: "Forest", src: 8, id: 8 },
+
+    { alt: "Sea Anemone 1", src: 21, id: 21 },
+    { alt: "Magenta Jellyfish", src: 22, id: 22 },
+    { alt: "King Crab", src: 23, id: 23 },
+    { alt: "Sea Anemone 2", src: 24, id: 24 },
+    { alt: "Sea Anemone 3", src: 25, id: 25 },
+    { alt: "School of Fishes", src: 26, id: 26 },
+    { alt: "Fishes that includes clown fishes", src: 27, id: 27 },
+    { alt: "School of Herring fish", src: 28, id: 28 },
+
+
+
     { alt: "Focus in 1 boat", src: 9, id: 9 },
     { alt: "Webworms", src: 14, id: 14 },
     { alt: "Stack rocks in a beach", src: 16, id: 16 },
     { alt: "Pebble beach on river", src: 17, id: 17 },
-    { alt: "Cave", src: 18, id: 18 },
-    { alt: "Cave", src: 19, id: 19 },
-    { alt: "Cave", src: 20, id: 20 },
+    { alt: "Cave 1", src: 18, id: 18 },
+    { alt: "Cave 2", src: 19, id: 19 },
+    { alt: "Cave 3", src: 20, id: 20 },
 ];
--- src/json/photographyList.js
+++ src/json/photographyList.js
@@ -1,33 +1,38 @@
 export const photography_list = [
-    { alt: "Fancy dining room in a birthday event", src: "p10a", id: 1 },
-    { alt: "Giant Teddy Bear sitting alone", src: "p11a", id: 2 },
-    { alt: "Poker table at Whitchurch-Stouffville, ON", src: "p12a", id: 3 },
 
-    { alt: "Pioneer Digital DJ System", src: "p18a", id: 4 },
-    { alt: "Persian chess set at the Multicultural Festival", src: "p14a", id: 5 },
-    { alt: "Gaming headset in our house", src: "p16a", id: 7 },
+    { alt: "Crocoite display in Royal Ontario Museum", src: "p7", id: 1 },
+    { alt: "Chinese Magnetic Mariners Compass (19th century)", src: "p8", id: 2 },
+    { alt: "Aetosaur - Fossil Dinosaur Skeleton", src: "p9", id: 3 },
 
-    { alt: "Deep fried quailed eggs coated w/ blue batter (Filipino dish)", src: "p15a", id: 6 },
-    { alt: "A tall birdhouse in St. Thomas park", src: "p17a", id: 8 },
-    { alt: "Japanese umbrella (Wagasa) taken during Autumn season", src: "p13a", id: 9 },
+    { alt: "Fancy dining room in a birthday event", src: "p10a", id: 4 },
+    { alt: "Giant Teddy Bear sitting alone", src: "p11a", id: 5 },
+    { alt: "Poker table at Whitchurch-Stouffville, ON", src: "p12a", id: 6 },
 
-    { alt: "Canada National Defense in Brantford, Ontario  ", src: "p26", id: 10 },
-    { alt: "Downtown Stratford, Ontario", src: "p23a", id: 11 },
-    { alt: "Outdoor skating rink in Toronto, Ontario", src: "p24a", id: 12 },
+    { alt: "Pioneer Digital DJ System", src: "p18a", id: 7 },
+    { alt: "Persian chess set at the Multicultural Festival", src: "p14a", id: 8 },
+    { alt: "Gaming headset in our house", src: "p16a", id: 9 },
 
-    { alt: "Portrait Of Joni Mitchell & some uknown art print", src: "p28", id: 13 },
-    { alt: "Driving in 407", src: "p29", id: 14 },
-    { alt: "Dragon Crest knife dagger", src: "p30", id: 15 },
+    { alt: "Deep fried quailed eggs coated w/ blue batter (Filipino dish)", src: "p15a", id: 10 },
+    { alt: "A tall birdhouse in St. Thomas park", src: "p17a", id: 11 },
+    { alt: "Japanese umbrella (Wagasa) taken during Autumn season", src: "p13a", id: 12 },
 
-    { alt: "A fox hat while walking in Killaly Park", src: "p31", id: 16 },
-    { alt: "Walking in the woods - Orange beanie focus", src: "p32", id: 17 },
-    { alt: "Brant Conservation Area", src: "p33", id: 18 },
+    { alt: "Canada National Defense in Brantford, Ontario  ", src: "p26", id: 13 },
+    { alt: "Downtown Stratford, Ontario", src: "p23a", id: 14 },
+    { alt: "Outdoor skating rink in Toronto, Ontario", src: "p24a", id: 15 },
 
-    { alt: "Wooden coffins dangle from cliffs in Sagada (Philippines)", src: "p19a", id: 19 },
-    { alt: "Two elderlies working in a Ginseng farm", src: "p20a", id: 20 },
-    { alt: "A cool stone in Niagara on the lake", src: "p21a", id: 21 },
+    { alt: "Portrait Of Joni Mitchell & some uknown art print", src: "p28", id: 16 },
+    { alt: "Driving in 407", src: "p29", id: 17 },
+    { alt: "Dragon Crest knife dagger", src: "p30", id: 18 },
 
-    { alt: "Brantford city hall", src: "p25", id: 22 },
-    { alt: "CN Tower", src: "p27", id: 23 },
-    { alt: "Brantford city hall", src: "p27a", id: 24 },
+    { alt: "A fox hat while walking in Killaly Park", src: "p31", id: 19 },
+    { alt: "Walking in the woods - Orange beanie focus", src: "p32", id: 20 },
+    { alt: "Brant Conservation Area", src: "p33", id: 21 },
+
+    { alt: "Wooden coffins dangle from cliffs in Sagada (Philippines)", src: "p19a", id: 22 },
+    { alt: "Two elderlies working in a Ginseng farm", src: "p20a", id: 23 },
+    { alt: "A cool stone in Niagara on the lake", src: "p21a", id: 24 },
+
+    { alt: "Brantford city hall", src: "p25", id: 25 },
+    { alt: "CN Tower", src: "p27", id: 26 },
+    { alt: "Brantford city hall", src: "p27a", id: 27 },
 ];
